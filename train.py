7784ec3b36ebc052813ad2bbeda17ea31225253f
Omer Ferhat <omerf.sarioglu@gmail.com>
2020-10-16 19:04:52 +0300

    callbacks added

--- train.py
+++ train.py
@@ -13,10 +13,27 @@ class Trainer(object):
         self.step_size_val = self.val_gen.n // self.val_gen.batch_size
 
     def train(self):
+        scheduler_callback = tf.keras.callbacks.LearningRateScheduler(self.scheduler)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir="logs/",
+            histogram_freq=1,
+            write_graph=True,
+            write_images=True,
+            update_freq="epoch",
+            profile_batch=10)
+
         self.model.fit(
             x=self.train_gen,
             steps_per_epoch=self.step_size_train,
             validation_data=self.val_gen,
             validation_steps=self.step_size_val,
             epochs=self.epoch,
+            callbacks=[scheduler_callback, tensorboard_callback]
         )
+
+    @staticmethod
+    def scheduler(epoch, lr):
+        if epoch < 15:
+            return lr
+        else:
+            return lr * tf.math.exp(-0.1)
